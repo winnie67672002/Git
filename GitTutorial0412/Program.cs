e7d25f85795f43c6cc0a55487ca0e60cce7218c6
Natasha <natasha.wang@shinda.com.tw>
2022-11-20 14:55:19 +0800

    after reset commit

--- GitTutorial0412/Program.cs
+++ GitTutorial0412/Program.cs
@@ -1,9 +1,12 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("!");
+Console.WriteLine("修改");
+Console.WriteLine("Head change again");
 Console.WriteLine("0412修改");Console.WriteLine("修改");
 Console.WriteLine("在master修改");
 Console.WriteLine("feature_1");
 Console.WriteLine("feature_1");
 Console.WriteLine("feature_1 after merge change again");
+Console.WriteLine("feature_1");
 Console.WriteLine("feature_1");
