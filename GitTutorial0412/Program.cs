66ac2b439e038f0a9cf165d6d332c0566c6a580f
Natasha <natasha.wang@shinda.com.tw>
2022-11-20 12:33:56 +0800

    both 衝突

--- GitTutorial0412/Program.cs
+++ GitTutorial0412/Program.cs
@@ -2,9 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("!");
 Console.WriteLine("0412修改");Console.WriteLine("修改");
-<<<<<<< HEAD
 Console.WriteLine("在master修改");
-=======
 Console.WriteLine("feature_1");
 Console.WriteLine("feature_1");
->>>>>>> feature_1
